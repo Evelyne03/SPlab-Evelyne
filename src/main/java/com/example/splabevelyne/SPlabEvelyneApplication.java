2ee4ea9f044fd8c544dba80da4d8cf356ed718da
Evelyne03 <evelyne.tudureanu03@e-uvt.ro>
2023-10-12 10:20:27 +0300

    Connected IntelliJ to Github

---
+++ src/main/java/com/example/splabevelyne/SPlabEvelyneApplication.java
@@ -0,0 +1,13 @@
+package com.example.splabevelyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPlabEvelyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SPlabEvelyneApplication.class, args);
+	}
+
+}
