9cf38415c499b1b60620b49c53ec22110d828204
Evelyne03 <evelyne.tudureanu03@e-uvt.ro>
2023-10-19 10:58:49 +0300

    Last version of lab 1

--- src/main/java/com/example/splabevelyne/SPlabEvelyneApplication.java
+++ src/main/java/com/example/splabevelyne/SPlabEvelyneApplication.java
@@ -3,31 +3,24 @@ package com.example.splabevelyne;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-@SpringBootApplication
+
 public class SPlabEvelyneApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SPlabEvelyneApplication.class, args);
-        Book discoTitanic = new Book("Disco Titanic");
-        Author author = new Author("Radu Pavel Gheo");
-        discoTitanic.addAuthor(author);
-        int IndexChapterOne = discoTitanic.createChapter("Chapter 1");
-        Chapter chp1 = discoTitanic.getChapter(1);
-        int indexSubChapterOneOne = chp1.createSubchapter("Subchapter 1.1");
-        Subchapter scOne0ne = chp1.getSubchapter(indexSubChapterOneOne);
-        //scOneOne.createNewParagraph("Paragraph 1");
-        //scOneOne.createNewParagraph("Paragraph 2");
-        //scOneOne.createNewParagraph("Paragraph 3");
-        //scOneOne.createNewImage("Image 1");
-        //scOneOne.createNewParagraph("Paragraph 4");
-        //ScOneOne.createNewTable("Table 1");
-        //scOneOne.createNewParagraph("Paragraph 5");
-        //scOneOne.print();
-    }
+        public static void main(String[] args) {
+                Book discoTitanic = new Book("Disco Titanic");
+                Author author = new Author("Radu Pavel Gheo");
+                discoTitanic.addAuthor(author);
+                int indexChapterOne = discoTitanic.createChapter("Capitolul 1");
+                Chapter chp1 = discoTitanic.getChapter(indexChapterOne);
+                int indexSubChapterOneOne = chp1.createSubchapter("Subcapitolul 1.1");
+                Subchapter scOneOne = chp1.getSubchapter(indexSubChapterOneOne);
+                scOneOne.createNewParagraph("Paragraph 1");
+                scOneOne.createNewParagraph("Paragraph 2");
+                scOneOne.createNewParagraph("Paragraph 3");
+                scOneOne.createNewImage("Image 1");
+                scOneOne.createNewParagraph("Paragraph 4");
+                scOneOne.createNewTable("Table 1");
+                scOneOne.createNewParagraph("Paragraph 5");
+                scOneOne.print();
+        }
 
 }
-
-
-
-
-
-
