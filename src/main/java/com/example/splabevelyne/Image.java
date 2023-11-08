08ee09095ce2086dcca4a741068c03e0995eb3d9
Tudureanu Evelyne-Andreea <evelyne.tudureanu03@e-uvt.ro>
2023-11-08 18:08:04 +0200

    implemented lab 4 Proxy

--- src/main/java/com/example/splabevelyne/Image.java
+++ src/main/java/com/example/splabevelyne/Image.java
@@ -1,28 +1,47 @@
 package com.example.splabevelyne;
 
-public class Image implements Element{
-    private String imagename;
-    Image(String imagename){
-        this.imagename = imagename;
+import java.util.concurrent.TimeUnit;
+
+public class Image implements Element {
+    private String name;
+    private String url;
+
+    Image(String url, String name) {
+        this.url = url;
+        this.name = name;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void print(){
-        System.out.println("Image with name: " + imagename);
-    }
+        Image(String url) {
+            this.url = url;
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-    @Override
-    public void add(Element e){
-        throw new UnsupportedOperationException();
-    }
+        @Override
+        public void print() {
+            System.out.println("Image with url: " + url);
+        }
 
-    @Override
-    public void removeElement(Element e){
-        throw new UnsupportedOperationException();
-    }
+        @Override
+        public void add(Element e){
+            throw new UnsupportedOperationException();
+        }
 
-    @Override
-    public Element get(int i){
-        throw new UnsupportedOperationException();
+        @Override
+        public void removeElement(Element e){
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Element get(int i){
+            throw new UnsupportedOperationException();
+        }
     }
-}
---
+++ src/main/java/com/example/splabevelyne/Picture.java
@@ -0,0 +1,8 @@
+package com.example.splabevelyne;
+
+import java.awt.*;
+public interface Picture {
+    String url();
+    Dimension dim();
+    String content();
+}
